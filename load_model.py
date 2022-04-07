f4fbfd28ae154476992c13bf2b3a37f0de0a7afc
Shivam Patil <92985763+ShivamVPatil@users.noreply.github.com>
2022-04-07 20:43:06 +0530

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -10,8 +10,7 @@ def get_model(model_path):
     except:
         print("Cannot fetch model from local downloading from drive")
         if not 'model.pkl' in os.listdir('.'):
-            # example url: "https://drive.google.com/u/1/uc?id=18IxYOI-whucBTZmt5qTvvYgjlxleaSqO&export=download&confirm=t"
-            url = "https://drive.google.com/file/d/17BSrJSDmL7m5ipyK0fEezK42MPouw0bR/view?usp=sharing"
+            url = "https://drive.google.com/uc?id=17BSrJSDmL7m5ipyK0fEezK42MPouw0bR&export=download"
             r = requests.get(url, allow_redirects=True)
             open(r"model.pkl", 'wb').write(r.content)
             del r
